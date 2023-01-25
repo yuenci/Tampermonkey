a70a1ac8ad5cc50d1cf0fe2e4831f8670571712b
Innis <1575270674@qq.com>
2023-01-25 19:02:45 +0800

    init commit

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Innis script 1
+// @namespace    https://www.enjoycoding.me/
+// @version      0.1
+// @description  hello new world!
+// @author       Innis
+// @match         *://*
+// @icon         https://utoolsfigurebed.oss-cn-hangzhou.aliyuncs.com/logoIcon.png
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    console.log('Hello World!');
+})();
